621222c176ae4309a1387bc213a0d54b04fcf1dd
AkioMabuchi <mabuchiakio@gmail.com>
2022-05-06 22:49:26 +0900

    Fixed bug

--- Assets/Scripts/TwitterModel.cs
+++ Assets/Scripts/TwitterModel.cs
@@ -8,12 +8,16 @@ public static class TwitterModel
 {
     [DllImport("__Internal")]
     private static extern void OpenNewWindow(string openUrl);
-    
+
     public static void Tweet(string text)
     {
         var url = "https://twitter.com/intent/tweet?text=" + UnityWebRequest.EscapeURL(text);
-#if UNITY_EDITOR && UNITY_WEBGL
+#if UNITY_EDITOR
+        Application.OpenURL(url);
+# elif UNITY_WEBGL
         OpenNewWindow(url);
+#else
+        Application.OpenURL(url);
 #endif
     }
 }
